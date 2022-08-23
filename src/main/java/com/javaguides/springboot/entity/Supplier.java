2de9c1269109e976445a1dfdcbdcc9358d94d27e
R1dZ2 <Ridhwaan09@hotmail.com>
2022-08-23 21:35:16 +0400

    Initial commit

---
+++ src/main/java/com/javaguides/springboot/entity/Supplier.java
@@ -0,0 +1,21 @@
+package com.javaguides.springboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Supplier {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+}
